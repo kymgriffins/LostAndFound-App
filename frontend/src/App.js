b2c3e276933a2157e7585d1b3bdd6594fcb332d0
bryson-20 <bryankimtai330@gmail.com>
2022-03-31 10:28:09 +0300

    submit-items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import PrivateRoute from './utils/PrivateRoute'
-import { AuthProvider } from './context/AuthContext'
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PrivateRoute from "./utils/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
 
-import Dashboard from './pages/Dashboard'
-import LoginPage from './pages/LoginPage'
-import Header from './components/Header'
+import Dashboard from "./pages/Dashboard";
+import LoginPage from "./pages/LoginPage";
+import Header from "./components/Header";
+import SubmitItem from "./pages/SubmitItem";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <AuthProvider>
-         
-          <PrivateRoute component={Dashboard} path="/" exact/>
-          <Route component={LoginPage} path="/login"/>
+          <PrivateRoute component={Dashboard} path="/" exact />
+          <Route component={LoginPage} path="/login" />
+          <Route component={SubmitItem} path="/submit-item" />
         </AuthProvider>
       </Router>
     </div>
